2cd0e4c51f852c500ab7ed42f4607af7ff4728b1
apbrandom <apbrandom@gmail.com>
2023-08-07 19:15:52 +0200

    Initial Commit

---
+++ SuperHero3/SuperHero3App.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHero3App.swift
+//  SuperHero3
+//
+//  Created by Vadim Vinogradov on 07.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperHero3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
